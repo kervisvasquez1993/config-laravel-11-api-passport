884ae460242015a10af77e0a79d85fa39f1e2a27
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-04-16 23:47:17 -0400

    add login and register in route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
